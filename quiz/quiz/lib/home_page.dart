02c27db19f1e2c77ee1e9626ce173a8b7970ce28
alexj <jorgecalruiz6@gmail.com>
2023-02-21 15:07:20 -0800

    actualizacion

--- quiz/quiz/lib/home_page.dart
+++ quiz/quiz/lib/home_page.dart
@@ -26,9 +26,44 @@ class _QuizPageState extends State<QuizPage> {
   List<String> preguntas = [
     "¿Los globulos rojos viven 4 meses?",
     "El cuerpo humano adulto tiene 306 huesos?",
-    "La cobalamina es una vitamina"
+    "La cobalamina es una vitamina",
+    "true",
+    "false",
+    "false"
+  ];
+  List<bool> respuestas = [
+    true,
+    false,
+    true,
+    true,
+    false,
+    false,
   ];
   int numeroPregunta = 0;
+
+  respuesta() {
+    int i = 0;
+    bool respuestaCorrecta = respuestas[numeroPregunta];
+    if (respuestaCorrecta == respuestas[i]) {
+      puntuacion.add(
+        const Icon(
+          Icons.check,
+          color: Colors.green,
+          size: 40,
+        ),
+      );
+      numeroPregunta++;
+    } else if (respuestaCorrecta == respuestas[i]) {
+      puntuacion.add(
+        const Icon(
+          Icons.close,
+          color: Colors.red,
+          size: 40,
+        ),
+      );
+      numeroPregunta++;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -61,14 +96,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
                 onPressed: () {
                   setState(() {
-                    puntuacion.add(
-                      Icon(
-                        Icons.check,
-                        color: Colors.green,
-                        size: 40,
-                      ),
-                    );
-                    numeroPregunta++;
+                    respuesta();
                   });
                 },
               ),
@@ -88,14 +116,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
                 onPressed: () {
                   setState(() {
-                    puntuacion.add(
-                      Icon(
-                        Icons.close,
-                        color: Colors.red,
-                        size: 40,
-                      ),
-                    );
-                    numeroPregunta++;
+                    respuesta();
                   });
                 },
               ),
